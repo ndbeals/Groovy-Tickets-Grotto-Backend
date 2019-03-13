cf51bb81430205a0994ea202a0817762bee8a42f
Nathan Beals <nathan_beals@hotmail.com>
2019-03-13 04:19:42 -0400

    Started writing tests, updated junit

--- src/test/java/com/groovy_tickets_grotto/backend/AppTest.java
+++ src/test/java/com/groovy_tickets_grotto/backend/AppTest.java
@@ -3,6 +3,7 @@ package com.groovy_tickets_grotto.backend;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import junit.*;
 
 /**
  * Unit test for simple App.
